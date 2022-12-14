ed1be06ce117ff0c3aa10daa1bbce74c3a1b6431
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-12-14 11:46:54 +0700

    jn

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя', blank=False)
+    surname = models.CharField(max_length=120, verbose_name='Фамилия', blank=False)
+    patronymic = models.CharField(max_length=120, verbose_name='Отчество', blank=False)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', blank=False)
+    author = models.ForeignKey(Author, max_length=100, verbose_name='Автор', blank=False)
+    year_of_rel = models.IntegerField(verbose_name='Год выпуска', blank=False,
+                                    validators=[MinValueValidator(1000), MaxValueValidator(9999)])
